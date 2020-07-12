493d10661f904dafb3983dbe9f1238230bdf14b7
Lizhen Shi <lizhen09.shi@gmail.com>
2020-07-12 01:29:46 -0400

    fix label change forth and back issue

--- apps/label_propagation_rand_long_short_v1/label_propagation_rand_long_short_v1.cpp
+++ apps/label_propagation_rand_long_short_v1/label_propagation_rand_long_short_v1.cpp
@@ -31,36 +31,50 @@ bool graph_is_weighted = false;
 bool graph_is_directed = false;
 bool do_not_use_rand = false;
 
-// The vertex data is its label 
-typedef unsigned int vertex_data_type;
+// The vertex data is its label
+//typedef unsigned int vertex_data_type;
+typedef unsigned int label_data_type;
+typedef struct {
+	label_data_type label;
+	bool changed;
+	void save(graphlab::oarchive &oarc) const {
+		oarc << label << changed;
+	}
+
+	void load(graphlab::iarchive &iarc) {
+		iarc >> label >> changed;
+	}
+
+} vertex_data_type;
 typedef float edge_data_type;
 
 // Suppose the input is concat of two dataset, the first dataset is assume to be long samples. 
 //This number is the start of the second dataset (e.g. short samples)
-vertex_data_type second_min_node_id = -1;
+graphlab::vertex_id_type second_min_node_id = -1;
 
 struct label_counter {
-	std::map<vertex_data_type, edge_data_type> label_count;
-
+	std::map<label_data_type, edge_data_type> label_count;
+	bool nbr_changed = false;
 	label_counter() {
 	}
 
 	label_counter& operator+=(const label_counter &other) {
-		for (std::map<vertex_data_type, edge_data_type>::const_iterator iter =
+		for (std::map<label_data_type, edge_data_type>::const_iterator iter =
 				other.label_count.begin(); iter != other.label_count.end();
 				++iter) {
 			label_count[iter->first] += iter->second;
-		}
 
+		}
+		nbr_changed = nbr_changed || other.nbr_changed;
 		return *this;
 	}
 
 	void save(graphlab::oarchive &oarc) const {
-		oarc << label_count;
+		oarc << label_count << nbr_changed;
 	}
 
 	void load(graphlab::iarchive &iarc) {
-		iarc >> label_count;
+		iarc >> label_count >> nbr_changed;
 	}
 };
 
@@ -82,8 +96,8 @@ bool line_parser(graph_type &graph, const std::string &filename,
 		strm >> weight;
 	// insert this vertex with its label
 
-	graph.add_vertex(vid1, vid1);
-	graph.add_vertex(vid2, vid2);
+	graph.add_vertex(vid1, { vid1, true });
+	graph.add_vertex(vid2, { vid2, true });
 	graph.add_edge(vid1, vid2, weight);
 
 	return true;
@@ -107,9 +121,10 @@ public:
 		bool isEdgeSource = (vertex.id() == edge.source().id());
 		bool sourceIsFirst = (edge.source().id() < second_min_node_id);
 		bool targetIsFirst = (edge.target().id() < second_min_node_id);
-		vertex_data_type neighbor_label =
-				isEdgeSource ? edge.target().data() : edge.source().data();
 
+		const vertex_data_type &neighbor_data =
+				isEdgeSource ? edge.target().data() : edge.source().data();
+		label_data_type neighbor_label = neighbor_data.label;
 		/*
 		 * This version allows
 		 * 		label propagation from long to short
@@ -119,19 +134,25 @@ public:
 		//long to short
 		if (!isEdgeSource && sourceIsFirst && !targetIsFirst) { //src is long, target is short, neighbor is long, v is short
 			// make a label_counter and place the neighbor data in it
-			if (neighbor_label < second_min_node_id) //only long label is allowed
+			if (neighbor_label < second_min_node_id) {//only long label is allowed
 				counter.label_count[neighbor_label] = edge.data();
+				counter.nbr_changed = neighbor_data.changed;
+			}
 		} else if (isEdgeSource && !sourceIsFirst && targetIsFirst) { //src is short, target is long, neighbor is long, v is short
 			// make a label_counter and place the neighbor data in it
-			if (neighbor_label >= second_min_node_id) //only long label is allowed
+			if (neighbor_label >= second_min_node_id) {	//only long label is allowed
 				counter.label_count[neighbor_label] = edge.data();
+				counter.nbr_changed = neighbor_data.changed;
+			}
 		}
 
 		//long to long
 		if (targetIsFirst && sourceIsFirst) { //src is long, target is long, neighbor is long, v is long, neighbor is long
 			// make a label_counter and place the neighbor data in it
-			if (neighbor_label < second_min_node_id) //only long label is allowed
+			if (neighbor_label < second_min_node_id) {//only long label is allowed
 				counter.label_count[neighbor_label] = edge.data();
+				counter.nbr_changed = neighbor_data.changed;
+			}
 		}
 		// gather_type is a label counter, so += will add neighbor counts to the
 		// label_count map.
@@ -140,13 +161,16 @@ public:
 
 	void apply(icontext_type &context, vertex_type &vertex,
 			const gather_type &total) {
-
+		if (total.label_count.empty()) {
+			return;
+		}
 		edge_data_type maxCount = 0;
 
-		vertex_data_type maxLabel = vertex.data();
+		const vertex_data_type &vdata = vertex.data();
+		label_data_type maxLabel = vdata.label;
 
 		// Figure out which label of the vertex's neighbors' labels is most common
-		for (std::map<vertex_data_type, edge_data_type>::const_iterator iter =
+		for (std::map<label_data_type, edge_data_type>::const_iterator iter =
 				total.label_count.begin(); iter != total.label_count.end();
 				++iter) {
 			if (iter->second > maxCount) {
@@ -165,16 +189,17 @@ public:
 
 		// if maxLabel differs to vertex data, mark vertex as changed and update
 		// its data.
-		if (vertex.data() != maxLabel) {
+		if (vertex.data().label != maxLabel) {
 			changed = true;
 			if (rand01() > 0.4) {
-				vertex.data() = maxLabel;
+				vertex.data() = { maxLabel, total.nbr_changed };
 			}
 		} else if (maxLabel >= second_min_node_id) {
 			changed = true;
 		} else {
 			changed = false;
 		}
+		changed = changed || total.nbr_changed;
 
 	}
 
@@ -199,7 +224,7 @@ public:
 struct labelpropagation_writer {
 	std::string save_vertex(graph_type::vertex_type v) {
 		std::stringstream strm;
-		strm << v.id() << "\t" << v.data() << "\n";
+		strm << v.id() << "\t" << v.data().label << "\n";
 		return strm.str();
 	}
 	std::string save_edge(graph_type::edge_type e) {
